0e7e64277eeee75d51b9d9e40366e8c2f77a0a1e
Morrales <leormm23@gmail.com>
2023-06-15 13:25:13 -0600

    09 tuplas y sets

---
+++ tuplas_sets.py
@@ -0,0 +1,19 @@
+my_tuple = (1, "dos", 3.1, True)
+print(type(my_tuple))
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[-1])
+my_set = { "uno", "dos", "tres", "dos"}
+
+print(type(my_set))
+print(my_set)
+
+my_set.add(4)
+print(my_set)
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print(a.union(b))
+print(a.intersection(b))
+
