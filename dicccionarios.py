f7f66c7fe34f28d4bfa63cbc5cfc23b536079798
Morrales <leormm23@gmail.com>
2023-06-15 13:38:15 -0600

    10 Diccionarios

---
+++ dicccionarios.py
@@ -0,0 +1,17 @@
+team = {
+    "name": "city",
+    "country": "England",
+    "champios": 1, 
+    "players": ['halland', 'grealish'] 
+
+    
+}
+print(type(team))
+print(team)
+print(team["name"])
+print(team["players"])
+team["players"].append("kevin")
+team["name"] = "Manchester"
+team["leage"] = "Premier League"
+print(team)
+print(team)
