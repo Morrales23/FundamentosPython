65ec741aa0eeddb608d62bd4ccc80740b94135f3
Morrales <leormm23@gmail.com>
2023-06-15 13:57:24 -0600

    11 Ciclos

---
+++ ciclos.py
@@ -0,0 +1,31 @@
+i = 1
+num = 7 
+while i <= 10:
+    print(f"{ num } * { i } = { num * 1 }")
+    i += 1
+    
+    my_string = "hola a todos"
+    for letter in my_string:
+        print(letter, end=', ')
+        print()
+        
+        
+        my_list = ["uno", "dos", "tres", "cuadro"]
+        for item in my_list:
+            print(item, end=', ')
+        print()
+        
+        
+        for i in range(10):
+            print(1, end=', ')
+        print()
+            
+        for i in range(10, 20):
+            print(i, end=', ')
+        print()
+        
+        for i in range(10, 100, 10):
+            print(i, end=', s')
+        print()
+            
+        
